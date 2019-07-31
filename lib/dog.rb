ee652242cec2438dceda0bc4cae9338ee2584f2d
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-31 19:25:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,7 @@ class Dog
       binding.pry
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      dog = self.create()
+      dog = self.create(name: name, breed: breed)
     end
     dog
   end
